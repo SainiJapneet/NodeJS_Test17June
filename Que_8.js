ffe94eea01e7f75c7ce9b865c2f6aceacca0af86
japneet <japneetsaini084@gmail.com>
2023-06-17 09:32:44 +0530

    .

--- Que_8.js
+++ Que_8.js
@@ -3,3 +3,38 @@
 //- add data to a file
 //- append data to a file
 //- read an html file and display it on the browser
+
+var fs = require('fs');
+var http = require("http");
+
+//creating and adding data to the file
+fs.writeFile('./myFile.txt',"Data added", (err,data) =>{
+    if(err){
+        console.log("Something went wrong : " + err);
+    }
+    else{
+        console.log("File created. ");
+    }
+})
+
+//appending data to the file.
+fs.appendFile('./myFile.txt',"Data appended",(err,data)=>{
+    if(err){
+        console.log("Something went wrong : " + err);
+    }
+    else{
+        console.log("File appended");
+    }
+})
+
+http.createServer(function(req,res){
+
+    fs.readFile("myHtml.html", function (err,data){
+        res.writeHead(200,{
+            "Content-Type": "text/html",
+        });
+        res.write(data);
+        res.end("")
+    });
+
+}).listen(8080);
