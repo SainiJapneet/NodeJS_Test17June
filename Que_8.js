8a9dad2af02474d8dc72eb1c630e9fa3ffb88d47
japneet <japneetsaini084@gmail.com>
2023-06-17 09:13:10 +0530

    initial commit

---
+++ Que_8.js
@@ -0,0 +1,5 @@
+//Write a node js script using file system which will 
+//- create a new file 
+//- add data to a file
+//- append data to a file
+//- read an html file and display it on the browser
---
+++ Que_9.js
@@ -0,0 +1,2 @@
+//What is a URL. Break it down into several parts that the url consists of. 
+//Use URL module to write a nodejs script to show the breakdown parts of a url as well.
